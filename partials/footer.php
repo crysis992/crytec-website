30784825a0ccae6771b4ad54e633c28f4617dc9c
crysis992 <crysis992@gmail.com>
2022-10-02 05:23:16 +0200

    added github link

--- partials/footer.php
+++ partials/footer.php
@@ -1,14 +1,16 @@
 <div class="footer p-4 md:flex md:items-center md:justify-between md:p-6 z-0">
     <div>
         <ul class="flex justify-center items-center gap-2 text-2xl">
-            <li><a href="https://google.de" target="_blank"><i class="fa-brands fa-github"></i></a></li>
+            <li><a href="https://github.com/crysis992/crytec-website" target="_blank"><i
+                        class="fa-brands fa-github"></i></a></li>
             <li><a href="https://google.de" target="_blank"><i class="fa-brands fa-discord"></i></a></li>
             <li><a href="https://google.de" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
             <li><a href="https://google.de" target="_blank"><i class="fa-brands fa-twitter"></i></a></li>
         </ul>
     </div>
 
-    <span class="text-sm sm:text-center">© 2022 <a href="https://crytec.net" class="hover:underline">CryTec™</a>. All Rights Reserved.
+    <span class="text-sm sm:text-center">© 2022 <a href="https://crytec.net" class="hover:underline">CryTec™</a>. All
+        Rights Reserved.
     </span>
 
     <ul class="flex flex-wrap items-center mt-3 text-sm sm:mt-0">

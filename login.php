b9ac1733f83dcc9a62b3ae30021eea30e83d49b7
crysis992 <crysis992@gmail.com>
2022-10-06 02:25:29 +0200

    new login page design

--- login.php
+++ login.php
@@ -2,12 +2,59 @@
 include_once "partials/header.php";
 ?>
 
+<style>
+body {
+    background: linear-gradient(#141e30, #243b55);
+}
 
+.loginform {
+    background: rgba(0, 0, 0, .5);
+    box-sizing: border-box;
+    box-shadow: 0 15px 25px rgba(0, 0, 0, .6);
+    border-radius: 10px;
+}
+
+.user-box {
+    position: relative;
+}
+
+.user-box input {
+    width: 100%;
+    padding: 10px 0;
+    font-size: 16px;
+    color: #fff;
+    margin-bottom: 30px;
+    border: none;
+    border-bottom: 1px solid #fff;
+    outline: none;
+    background: transparent;
+    border-radius: 0;
+}
+
+.user-box label {
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 10px 0;
+    font-size: 16px;
+    color: #fff;
+    pointer-events: none;
+    transition: .5s;
+}
+
+.user-box input:focus~label,
+.user-box input:valid {
+    top: -20px;
+    left: 0;
+    color: #03e9f4;
+    font-size: 12px;
+}
+</style>
 
 
 <div class="loginform">
 
-    <div class="signup flex justify-center flex-col items-center gap-4">
+    <div class="flex justify-center flex-col items-center gap-4">
         <h2 class="font-extrabold text-5xl mb-16">Log in</h2>
 
 
@@ -32,9 +79,14 @@ include_once "partials/header.php";
         <?php endif ?>
 
         <form action="<?= ROOT_URL ?>inc/login.inc.php" method="POST" class="flex flex-col justify-center w-fit gap-5">
-
-            <input type="text" name="username_email" placeholder="Username or Email">
-            <input type="password" name="password" placeholder="Password">
+            <div class="user-box">
+                <input type="text" name="username_email" id="username">
+                <label for="username">Username or Email</label>
+            </div>
+            <div class="user-box">
+                <input type="password" name="password" id="password">
+                <label for="password">Password</label>
+            </div>
             <button class="btn green" name="submit" type="submit">Log in</button>
             <small>Don't have an account? <a href="signup.php" class="text-secondary hover:underline">Register
                     now</a></small>

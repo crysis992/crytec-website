3d56d0b58564a5649d17b1a86f59a5b60475f960
crysis992 <crysis992@gmail.com>
2022-10-12 01:57:02 +0200

    fixed author

--- index.php
+++ index.php
@@ -38,26 +38,26 @@ $post_result = mysqli_query($connection, $post_query);
 
 <!-- START Featured Post -->
 <?php if (mysqli_num_rows($featured_query) == 1) : ?>
-<section class="container mx-auto featured-post">
-    <div class="featured-thumbnail"><img src="<?= ROOT_URL ?>static/thumbnails/<?= $featured['thumbnail'] ?>"></div>
-    <div class="featured-post-info">
-        <a href="" class="btn">Technology</a>
-        <h2 class="post-title"><a href="post.html"><?= $featured['title'] ?></a></h2>
-        <p class="post-body"><?= $featured['body'] ?></p>
-        <div class="post-author">
-            <div class="post-author-avatar">
-                <img src="<?= ROOT_URL ?>static/images/<?= $user_avatars[$featured['author_id']] ?>">
-            </div>
-            <div class=" post-author-info">
-                <h5>By: <?= $users[$featured['author_id']] ?></h5>
-                <small><?= $featured['date']  ?></small>
+    <section class="container mx-auto featured-post">
+        <div class="featured-thumbnail"><img src="<?= ROOT_URL ?>static/thumbnails/<?= $featured['thumbnail'] ?>"></div>
+        <div class="featured-post-info">
+            <a href="" class="btn">Technology</a>
+            <h2 class="post-title"><a href="post.html"><?= $featured['title'] ?></a></h2>
+            <p class="post-body"><?= $featured['body'] ?></p>
+            <div class="post-author">
+                <div class="post-author-avatar">
+                    <img src="<?= ROOT_URL ?>static/images/<?= $user_avatars[$featured['author_id']] ?>">
+                </div>
+                <div class=" post-author-info">
+                    <h5>By: <?= $users[$featured['author_id']] ?></h5>
+                    <small><?= $featured['date']  ?></small>
+                </div>
             </div>
         </div>
-    </div>
 
 
 
-</section>
+    </section>
 
 <?php endif ?>
 <!-- END Featured Post -->
@@ -70,34 +70,34 @@ $post_result = mysqli_query($connection, $post_query);
 
     <?php while ($post = mysqli_fetch_assoc($post_result)) : ?>
 
-    <article class="post-article w-[300px] md:w-[355px] mb-10 md:mb-0">
-        <div class="post-thumbnail"> <img src="<?= ROOT_URL ?>static/thumbnails/<?= $post['thumbnail'] ?>"> </div>
+        <article class="post-article w-[300px] md:w-[355px] mb-10 md:mb-0">
+            <div class="post-thumbnail"> <img src="<?= ROOT_URL ?>static/thumbnails/<?= $post['thumbnail'] ?>"> </div>
 
-        <div class="post-info">
-            <div class="flex gap-6 mb-4">
-                <a href="" class="btn"><?= $category_names[$post['category_id']] ?></a>
-                <h2><a href="post.html"><?= $post['title'] ?></a></h2>
-            </div>
-            <div class="post-body hidden md:block">
-                <p><?= $post['body'] ?></p>
-            </div>
+            <div class="post-info">
+                <div class="flex gap-6 mb-4">
+                    <a href="" class="btn"><?= $category_names[$post['category_id']] ?></a>
+                    <h2><a href="post.html"><?= $post['title'] ?></a></h2>
+                </div>
+                <div class="post-body hidden md:block">
+                    <p><?= $post['body'] ?></p>
+                </div>
 
-        </div>
-
-        <div class="post-author">
-            <div class="post-author-avatar">
-                <img src="<?= ROOT_URL ?>static/images/<?= $user_avatars[$featured['author_id']] ?>">
             </div>
 
-            <div class=" post-author-info">
-                <h5>By: <?= $users[$featured['author_id']] ?></h5>
-                <small><?= $featured['date']  ?></small>
+            <div class="post-author">
+                <div class="post-author-avatar">
+                    <img src="<?= ROOT_URL ?>static/images/<?= $user_avatars[$post['author_id']] ?>">
+                </div>
+
+                <div class=" post-author-info">
+                    <h5>By: <?= $users[$post['author_id']] ?></h5>
+                    <small><?= $post['date']  ?></small>
+                </div>
+
             </div>
 
-        </div>
 
-
-    </article>
+        </article>
 
     <?php endwhile ?>
 
@@ -109,11 +109,10 @@ $post_result = mysqli_query($connection, $post_query);
 
 <!-- Category Buttons -->
 <section class="relative category-buttons px-14 mx-auto flex justify-center py-5 mb-5 md:w-7/12">
-    <div
-        class="container category-button-container font-bold flex flex-wrap flex-grow items-center justify-center gap-2 pb-3">
+    <div class="container category-button-container font-bold flex flex-wrap flex-grow items-center justify-center gap-2 pb-3">
 
         <?php foreach ($category_names as $cat => $v) : ?>
-        <a href="" class="btn"><?= $v ?></a>
+            <a href="" class="btn"><?= $v ?></a>
         <?php endforeach ?>
 
     </div>
